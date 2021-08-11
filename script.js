0dcf30f5b80c9c3e96c2db3964832c9d7cf9c692
Jozeanne Belomo <jozeanne.belomo@gmail.com>
2021-08-10 21:44:50 -0300

    criar cobrinha

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [{ x: 8 * box, y: 8 * box }];
+let contador;
 
 function criarBackGround() {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function criarCobrinha() {
+  for (contador = 0; contador < snake.length; contador++) {
+    context.fillStyle = "green";
+    context.fillRect(snake[contador].x, snake[contador].y, box, box);
+  }
+}
+
 criarBackGround();
+criarCobrinha();
