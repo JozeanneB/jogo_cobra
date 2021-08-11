8477a419ea967e5bbd91cbbcbba2250566beed7b
Jozeanne Belomo <jozeanne.belomo@gmail.com>
2021-08-10 23:47:04 -0300

    adiciona game over e move comidinha após consumo

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
-let snake = [{ x: 8 * box, y: 8 * box }];
-let counter;
+let snake = [];
+snake[0] = { x: 8 * box, y: 8 * box };
 let direction = "right";
 let food = {
   x: Math.floor(Math.random() * 15 + 1) * box,
@@ -37,10 +37,17 @@ function update(event) {
 
 function iniciarJogo() {
   if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 + box;
+  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
 
   if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
   if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+  for (counter = 1; counter < snake.length; counter++) {
+    if (snake[0].x == snake[counter].x && snake[0].y == snake[counter].y) {
+      clearInterval(jogo);
+      alert("Game Over :(");
+    }
+  }
 
   criarBackGround();
   criarCobrinha();
@@ -54,7 +61,12 @@ function iniciarJogo() {
   if (direction == "up") positionY -= box;
   if (direction == "down") positionY += box;
 
-  snake.pop();
+  if (positionX != food.x || positionY != food.y) {
+    snake.pop();
+  } else {
+    food.x = Math.floor(Math.random() * 15 + 1) * box;
+    food.y = Math.floor(Math.random() * 15 + 1) * box;
+  }
 
   let newHead = { x: positionX, y: positionY };
 
